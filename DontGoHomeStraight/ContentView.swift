18502d0a4621d95cd136a9c48b7b0016d8cdbe73
Kazunori Sakata <sakata1977@gmail.com>
2025-08-05 18:24:35 +0900

    Initial Commit

---
+++ DontGoHomeStraight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DontGoHomeStraight
+//
+//  Created by kazunori.sakata.ts on 2025/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DontGoHomeStraight/DontGoHomeStraightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontGoHomeStraightApp.swift
+//  DontGoHomeStraight
+//
+//  Created by kazunori.sakata.ts on 2025/08/05.
+//
+
+import SwiftUI
+
+@main
+struct DontGoHomeStraightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
